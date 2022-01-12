e75120f943a82cf20287348dc3e95b1279e701d7
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2022-01-12 20:38:36 +0400

    Try

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Navbar from './Navbar';
+import Navbar from './Navbar';
 import Footer from './Footer';
 
 function Layout({ children }) {
   return (
     <React.Fragment>
-      {/* <Navbar /> */}
-      {children}
+      <Navbar />
+      <div>{children}</div>
       <Footer />
     </React.Fragment>
   );
